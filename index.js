a072a4f28f2095c390e489d9803e0592fa83951f
PREMYPRASAD <mailtopremy2010@gmail.com>
2022-11-16 01:33:58 +0530

    change

--- index.js
+++ index.js
@@ -4,7 +4,7 @@ const cors = require('cors');
 const path = require('path');
 var app = new express();
 //var Port = 3002;
-var Port = process.env.PORT || 1337;
+var Port = process.env.PORT || 3002;
 app.use(cors());
 app.use(express.static('./dist/frontend'));
 app.get("/api/Blogs", function(req, res) {
